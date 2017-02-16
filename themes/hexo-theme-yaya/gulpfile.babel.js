045ae30adcc488df732f4bb226afbb5428802413
mark <zqinmiao@gmail.com>
2017-02-16 21:56:19 +0800

    update

--- themes/hexo-theme-yaya/gulpfile.babel.js
+++ themes/hexo-theme-yaya/gulpfile.babel.js
@@ -3,11 +3,11 @@ import less from 'gulp-less';
 import cleanCss from 'gulp-clean-css';
 import autoprefixer from 'gulp-autoprefixer';
 
-gulp.task('less', ()=>gulp.src('./source/less/style.less')
-        .pipe(less())
-        .pipe(autoprefixer())
-        .pipe(cleanCss())
-        .pipe(gulp.dest('./source/css'))
+gulp.task('less', ()=>gulp.src('./src/less/style.less')
+    .pipe(less())
+    .pipe(autoprefixer())
+    .pipe(cleanCss())
+    .pipe(gulp.dest('./source/css'))
 );
 
-gulp.task('default', ['less'], ()=>gulp.watch('./source/less/**/*.less', ['less']));
+gulp.task('default', ['less'], ()=>gulp.watch('./src/less/**/*.less', ['less']));
