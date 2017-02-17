d185e5740200dcbb551d0636b96d7caf91c38f31
mark <564777189@qq.com>
2017-02-17 18:31:45 +0800

    update

--- themes/hexo-theme-yaya/gulpfile.babel.js
+++ themes/hexo-theme-yaya/gulpfile.babel.js
@@ -3,16 +3,21 @@ import less from 'gulp-less';
 import cleanCss from 'gulp-clean-css';
 import autoprefixer from 'gulp-autoprefixer';
 import del from 'del';
+import sequence from 'gulp-sequence';
 
 // 清理目录
 gulp.task('clean', () => del(["./source/css"], {dot: true}));
 
-//编译less
-gulp.task('less',["clean"], ()=>gulp.src('./src/less/style.less')
+//处理less
+gulp.task('less', ()=>gulp.src('./src/less/style.less')
     .pipe(less())
     .pipe(autoprefixer())
     .pipe(cleanCss())
     .pipe(gulp.dest('./source/css'))
 );
 
-gulp.task('default', ['less'], ()=>gulp.watch('./src/less/**/*.less', ['less']));
+gulp.task('watch',()=>gulp.watch('./src/less/**/*.less', ['less']));
+
+gulp.task('default', (cb)=>sequence('clean','less',cb));
+
+gulp.task('dev',['less','watch'],(cb)=>(cb));
